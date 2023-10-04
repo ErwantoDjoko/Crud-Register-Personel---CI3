106082ec8348c471262b232b04e4020bc661c365
Erwantojoko <joescore008@gmail.com>
2023-10-04 09:43:04 +0700

    Update routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'views';
+$route['default_controller'] = 'Entrys';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 $route['regis-personel'] = 'Entrys';
